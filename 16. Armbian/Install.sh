1a6a60ea270cb9e8153ce45348e15e3648dec72a
José Humberto Ferreira Júnior <josehumberto.fj@gmail.com>
2026-03-02 12:16:32 -0300

    Create Install.sh

---
+++ 16. Armbian/Install.sh
@@ -0,0 +1,21 @@
+
+curl -sSL https://raw.githubusercontent.com/urbancompasspony/bashrc/main/install.sh | bash
+
+sudo mkdir /home/administrador
+sudo chmod 777 -R /home/administrador
+
+sudo apt install docker-buildx docker.io \
+filelight smb4k plasma-disks kolourpaint granatier okteta krename timeshift kget gnome-nettool \
+iperf traceroute arp-scan speedtest-cli samba sshpass curl rclone openvpn qbittorrent nmap yt-dlp \
+cronie zip unzip dialog btrfs-progs cifs-utils inxi btop iotop cmatrix parallel rdesktop rpi-imager webp-pixbuf-loader gthumb \
+tree gparted gnome-disk-utility  audacity thunderbird libreoffice vlc kpat gimp remmina \
+terminator distrobox -y
+
+sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_arm64 -O /usr/local/bin/yq
+sudo chmod +x /usr/local/bin/yq
+
+
+
+> VULKAN
+For Armbian Noble KDE Neon 25.5.1
+sudo apt install mesa-vulkan-drivers mesa-utils vulkan-tools
